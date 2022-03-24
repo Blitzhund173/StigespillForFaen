71d52d183ec4c236a40595e4cbe439c05165f6de
blitz <blitz@blitzhund.com>
2022-03-24 14:36:03 +0100

    Update main

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-import sys,pygame, os
+import sys,pygame, os, time
 pygame.init()
 
-size=width, height= 1080, 1080
-speed=[2,2]
+size=width, height= 720, 720
+
 black=0, 0, 0
+white=255,255,255
+red=255,0,0
+blue=0,0,255
+green=0,255,0
+yellow=255,255,0
 
-screen=pygame.display.set_mode(size), pygame.SCALED
+screen=pygame.display.set_mode(size)#, pygame.SCALED
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    screen.fill(black)
+    pygame.display.flip()
+    time.sleep(0.01666)
+    screen.fill(white)
+    pygame.display.flip()
+    time.sleep(0.01666)
+    screen.fill(red)
+    pygame.display.flip()
+    time.sleep(0.01666)
+    screen.fill(blue)
+    pygame.display.flip()
+    time.sleep(0.01666)
+    screen.fill(green)
+    pygame.display.flip()
+    time.sleep(0.01666)
+    screen.fill(yellow)
+    pygame.display.flip()
+
+print("det funker for faen")
