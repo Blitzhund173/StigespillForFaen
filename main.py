b46d07e72d509d2edf19ce4fd49da20281690595
blitz <blitz@blitzhund.com>
2022-03-29 15:35:16 +0200

    Do much more

--- main.py
+++ main.py
@@ -12,19 +12,25 @@ red=255,0,0
 blue=0,0,255
 green=0,255,0
 yellow=255,255,0
+colorList=[red,blue,yellow,green,white,black]
 
 #Some playerdata, such as the current player, their position on the board (number they are on) and the x and y coordinates
 currentPlayer=0
-currentPlayerPos=[0,0,0,0]
+currentPlayerPos=[1,1,1,1]
 xCoords=[0]
 yCoords=[0]
+ladderList=[3,7,25,41,70,85,88,96]
+endLadderList=[45,13,67,80,51,29,56,77]
+endLadderX=[4,7,6,0,8,4,3]
+endLadderY=[5,8,3,2,7,4,2]
 
 #Stores blocks that are a different color
-redBlocks=[0]
-blueBlocks=[0]
-yellowBlocks=[0]
-greenBlocks=[0]
+redBlocks=[]
+blueBlocks=[]
+yellowBlocks=[]
+greenBlocks=[]
 
+colorBlockList=[redBlocks,blueBlocks,yellowBlocks,greenBlocks]
 
 #Actually draws the window to the screen
 screen=pygame.display.set_mode(size)
@@ -51,6 +57,10 @@ greenPlayer=pygame.image.load("Assets\Bøtte-Grønn.png")
 greenPlayer=pygame.transform.scale(greenPlayer,(70,70))
 greenRect=greenPlayer.get_rect()
 
+#List for the rects for each color
+playerList=[redPlayer,bluePlayer,yellowPlayer,greenPlayer]
+rectList=[redRect,blueRect,yellowRect,greenRect]
+
 #Defines drawSquare(). This is a function that draws a square in the designated position and color, is fixed to 70px x 70px
 def drawSquare(color,posX,posY):
     pygame.draw.rect(screen,color,pygame.Rect(posX,posY,70,70))
@@ -64,32 +74,48 @@ def drawNumber(text,posX,posY):
     pygame.display.flip()
 
 def move(num):
-    if currentPlayer==0:
-        for i in range(0,num):
-            currentPlayerPos[0]+=1
-            redRect.topleft=xCoords[currentPlayerPos[0]]*72+1,yCoords[currentPlayerPos[0]]*72+1
-            screen.blit(redPlayer,redRect)
-            drawSquare(white,xCoords[currentPlayerPos[0]-1]*72+1,yCoords[currentPlayerPos[0]-1]*72+1)
-            time.sleep(1)
+    global currentPlayer
+    for i in range(0,num):
+        time.sleep(0.5)
+        currentPlayerPos[currentPlayer]+=1
+        rectList[currentPlayer].topleft=xCoords[currentPlayerPos[currentPlayer]]*72+1,yCoords[currentPlayerPos[currentPlayer]]*72+1
+        screen.blit(playerList[currentPlayer],rectList[currentPlayer])
+        drawSquare(colorList[currentPlayer],xCoords[currentPlayerPos[currentPlayer]]*72+1,yCoords[currentPlayerPos[currentPlayer]]*72+1)
+        drawNumber(f"{currentPlayerPos[currentPlayer]}",xCoords[currentPlayerPos[currentPlayer]]*72+1,yCoords[currentPlayerPos[currentPlayer]]*72+1)
+        drawSquare(colorList[currentPlayer],xCoords[currentPlayerPos[currentPlayer]-1]*72+1,yCoords[currentPlayerPos[currentPlayer]-1]*72+1)
+        drawNumber(f"{currentPlayerPos[currentPlayer]-1}",xCoords[currentPlayerPos[currentPlayer]-1]*72+1,yCoords[currentPlayerPos[currentPlayer]-1]*72+1)
+        colorBlockList[currentPlayer].append(currentPlayerPos)
+        for i in range(0,4):
+            screen.blit(playerList[i],rectList[i])
+            pygame.display.flip()
+    
+    if currentPlayerPos[currentPlayer] in ladderList:
+        currentPlayerPos[currentPlayer]=endLadderList[int(ladderList.index(currentPlayerPos[currentPlayer]))]
         
-    if currentPlayer==1:
-        pass
-    if currentPlayer==2:
-        pass
-    if currentPlayer==3:
-        pass
+
+    for i in range(0,4):
+        screen.blit(playerList[i],rectList[i])
+    if currentPlayer>=3:
+        currentPlayer=0
+    else:
+        currentPlayer+=1
+
 
 dices=[]
 diceRect=[]
-for i in range(1,6):
+for i in range(1,7):
     dices.append(pygame.image.load(f"Assets/{i}.png"))
     diceRect.append(dices[i-1].get_rect())
-
+    diceRect[i-1].center=360,360
 
 def diceRoll():
-    for i in range(1,6):
-        pass
-    return random.randint(1,6)
+    for i in range(0,6):
+        rand=random.randint(0,5)
+        print(rand)
+        screen.blit(dices[rand],diceRect[rand])
+        pygame.display.flip()
+        time.sleep(0.5)
+    return rand+1
 
 screen.fill(black)
 #Draws all the squares and constructs the coordinate list
@@ -123,8 +149,6 @@ for y in range(0,10):
         for x in range(0,10):
             numberToWrite+=1
             drawNumber(numberToWrite,x*72+1,y*72+1)
-
-redRect.move(10,0)
 
 redRect.topleft=xCoords[1]*72+1,yCoords[1]*72+1
 screen.blit(redPlayer,redRect)
